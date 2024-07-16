1908c734324eaf66c7dc64910ba5cdf62da49688
Yashgabani845 <gabaniyash845@gmail.com>
2024-07-16 23:28:51 +0530

    depenendency added

--- Codemate/src/App.js
+++ Codemate/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <Navbar/>
   );
 }
 
