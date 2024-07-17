197653d869539d6c9470e37057cb96974fd676d0
Yashgabani845 <gabaniyash845@gmail.com>
2024-07-17 12:09:24 +0530

    required componenet made

--- Codemate/src/App.js
+++ Codemate/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import MainView from './Components/MainView';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <Navbar/>
+    <><Navbar />
+    <MainView /></>
   );
 }
 
---
+++ Codemate/src/Components/Docspage.jsx
@@ -0,0 +1,9 @@
+import React  from "react";
+const Docspage= ()=>{
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default Docspage;
